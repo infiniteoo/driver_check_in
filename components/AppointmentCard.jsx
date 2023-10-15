2381785e0fa810f115d0c2e0563b6e48357b565f
infiniteoo <troydorman@gmail.com>
2023-10-15 13:52:55 -0600

    not rendering buttons if order is complete

--- components/AppointmentCard.jsx
+++ components/AppointmentCard.jsx
@@ -89,30 +89,34 @@ const AppointmentRow = ({ appointment }) => {
         <div className="p-2 flex-1">{appointment.status}</div>
       </div>
       <div className="flex flex-row">
-        <select
-          className="p-2"
-          value={selectedAssignee}
-          onChange={handleAssignChange}
-        >
-          <option value="">Select Assignee</option>
-          {OPERATORS.map((operator) => (
-            <option key={operator} value={operator}>
-              {operator}
-            </option>
-          ))}
-        </select>
+        {appointment.status !== "Complete" ? (
+          <>
+            <select
+              className="p-2"
+              value={selectedAssignee}
+              onChange={handleAssignChange}
+            >
+              <option value="">Select Assignee</option>
+              {OPERATORS.map((operator) => (
+                <option key={operator} value={operator}>
+                  {operator}
+                </option>
+              ))}
+            </select>
 
-        <button
-          className={`p-2 bg-starfield3 text-starfield5 rounded m-5 ${
-            appointment.status === "Loading" ? "bg-starfield4" : ""
-          }`}
-          onClick={handleAction}
-        >
-          {appointment.status === "Loading" ? "Complete" : "Loading"}
-        </button>
-        <button className="p-2 bg-starfield2 text-starfield5 rounded m-5">
-          Cancel
-        </button>
+            <button
+              className={`p-2 bg-starfield3 text-starfield5 rounded m-5 ${
+                appointment.status === "Loading" ? "bg-starfield4" : ""
+              }`}
+              onClick={handleAction}
+            >
+              {appointment.status === "Loading" ? "Complete" : "Loading"}
+            </button>
+            <button className="p-2 bg-starfield2 text-starfield5 rounded m-5">
+              Cancel
+            </button>
+          </>
+        ) : null}
       </div>
     </div>
   );
